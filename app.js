e85d8a62dabc42ee1da7dc8235ad5443483c866b
Pjangwon <pjw051016@gmail.com>
2024-08-03 12:52:15 +0900

    기본적인 서버셋팅

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const dotenv = require('dotenv');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.join());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 존재하지 않음`);
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE.env !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`${app.get('port')}에서 서버 작동중`);
+});
